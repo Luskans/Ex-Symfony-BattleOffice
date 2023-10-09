0c32d7ec40fb1be256a92756465f7643a97e0116
Luskans <sylvain.migne@gmail.com>
2023-10-09 17:13:39 +0200

    add form types

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -2,6 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Entity\Client;
+use App\Entity\Country;
+use App\Entity\Order;
+use App\Entity\PaymentMethod;
+use App\Entity\Product;
+use App\Entity\Status;
+use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,12 +20,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class LandingPageController extends AbstractController
 {
     #[Route('/', name: 'landing_page')]
-    public function index(Request $request)
+    public function index(Request $request, EntityManagerInterface $entityManager, Client $client, Address $address, Country $country, PaymentMethod $paymentMethod, Product $product, Status $status, Order $order)
     {
-        //Your code here
+        $form = $this->createForm(OrderType::class, $order);
 
         return $this->render('landing_page/index_new.html.twig', [
-
+            'oderForm' => $form
         ]);
     }
     
